9fce536b88fcd222a9b19f3c015b2ff44aa3aa05
irincovictor-cmd <irinco.victor@cec.edu.ph>
2026-08-25 16:19:56 +0800

    Organize routes: consistent naming, grouping, and clean comments

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
-use App\Http\Controllers\StudentController;
+
 use App\Http\Controllers\OperatorController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
 
-// Redirect root URL to /home
+// Portfolio pages
 Route::get('/', function () {
     return redirect()->route('home');
 });
@@ -25,15 +31,11 @@ Route::get('/contact', function () {
     return view('portfolio', ['section' => 'contact']);
 })->name('contact');
 
-// Student Log in form 
+// Student form
+Route::get('/student', [StudentController::class, 'index'])->name('student.index');
+Route::post('/student', [StudentController::class, 'show'])->name('student.show');
 
-Route::get('student',[StudentController::class,'index']);
-Route::post('student',[StudentController::class, 'show']);
-
-Route::get('/operator',[OperatorController::class, 'index'])->name('operator.index');
-
+// Calculator (Operator)
+Route::get('/operator', [OperatorController::class, 'index'])->name('operator.index');
 Route::get('/operator/{type}', [OperatorController::class, 'showForm'])->name('operator.show');
-
 Route::post('/operator/{type}', [OperatorController::class, 'calculate'])->name('operator.calculate');
-
-
